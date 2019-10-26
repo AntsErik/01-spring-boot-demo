3ec52a2859e033269e91d845fabd7ce444f3123f
AntsErik <ants.erik.noormagi@gmail.com>
2019-10-26 16:54:54 +0300

    Adding BasePackages to scan reference to main method.

--- src/main/java/ee/praktika/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/ee/praktika/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,7 +3,8 @@ package ee.praktika.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages=("ee.praktika.springboot.demo.mycoolapp"))
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
